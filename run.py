8c8cbc0f7c4054222acce9b211dbb4b54b056231
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-21 14:28:14 +0300

    fix indentation error

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from user import User
 
-    def create_user(account,username,password):
-        '''
-        Function to create a new user
-        '''
-        new_user = User(account,username,password)
-        return new_user
+def create_user(account,username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(account,username,password)
+    return new_user
+
