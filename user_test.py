35c7dc4d4c02d3f315b471586cf2f47673e931af
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-21 11:49:16 +0300

    added setUp method

--- user_test.py
+++ user_test.py
@@ -1,2 +1,20 @@
 import unittest # Importing unittest
 from user import User # Importing the User class
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+         unittest.TestCase:TestCase that help in creating test cases
+    """
+
+    def setUp(self):
+        """
+        set up method to run before each test cases.
+        """
+        #
+        self.new_user = User("facebook","biron odhiambo","biron 4745") # Create user object
+
+
+        
