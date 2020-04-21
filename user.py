4a762261ec29297d3cbaff9becc6e98a984ec4ea
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-21 10:18:51 +0300

    initialized the user objects

--- user.py
+++ user.py
@@ -2,4 +2,10 @@ class User:
     """ 
     Class that generate new instance of user.
     """
-    
+    def __init__(self, account_name, login_username, user_password):
+        '''
+        Initializing the variables
+        '''
+        self.account_name = account_name
+        self.login_username = login_username
+        self.user_password = user_password
