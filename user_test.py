8bbe61d7a9dfaa985b966fac7169c07849c9d8e6
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-21 14:00:42 +0300

    display users test method

--- user_test.py
+++ user_test.py
@@ -84,6 +84,12 @@ class TestUser(unittest.TestCase):
         user_exists = User.user_exist("biron odhiambo")
         self.assertTrue(user_exists)
 
+    def test_display_all_users(self):
+        '''
+        method that returns a list of all users saved
+        '''
+        self.assertEqual(User.display_users(),User.user_list)
+
 
 
 
