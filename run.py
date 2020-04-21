8496065cbf5a0620d7faa71475804243c721a3e7
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-21 14:41:55 +0300

    method that finds the user by there account name

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from user import User
 
-def create_user(account,username,password):
+def create_user(account_name,username,password):
     '''
     Function to create a new user
     '''
-    new_user = User(account,username,password)
+    new_user = User(account_name,username,password)
     return new_user
 
 def save_users(user):
@@ -19,4 +19,8 @@ def del_user(user):
     '''
     user.delete_user()
 
-    
+def find_user(account_name):
+    '''
+    Function that finds a user by username and returns the user
+    '''
+    return User.find_by_account_name(account_name)
