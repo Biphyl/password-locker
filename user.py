d0f0115601d68cf9277c02904b4760e8f43d2af8
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-21 12:09:47 +0300

    save_user test and save_user method

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ class User:
     """ 
     Class that generate new instance of user.
     """
+    user_list = [] #Empty user list
+
     def __init__(self, account_name, login_username, user_password):
         '''
         Initializing the variables
@@ -9,3 +11,10 @@ class User:
         self.account_name = account_name
         self.login_username = login_username
         self.user_password = user_password
+
+    def save_user(self):
+        '''
+        save_user method saves user object into user_list
+        '''
+
+        User.user_list.append(self)
--- user_test.py
+++ user_test.py
@@ -26,5 +26,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.user_password,"biron 4745")
 
 
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+
+
 if __name__=='__main__':
     unittest.main()
